015570286cd2c6db0020b196b5222f951f872282
Marcin <marcinrszalek@gmail.com>
2017-05-15 22:57:23 +0200

    1com

---
+++ script.js
@@ -0,0 +1,15 @@
+var list = document.getElementById('list');
+var add = document.getElementById('button');
+
+add.addEventListener('click', function() {
+    var element = document.createElement('li');
+
+});
+
+var listElement = document.getElementsByTagName = ('li');
+element.innerHTML = 'item';
+var countOfListElement = listElement.lenght;
+
+list.appendChild(element);
+console.log(countOfListElement);
+
